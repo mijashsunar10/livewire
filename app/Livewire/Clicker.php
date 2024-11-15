96188fdcd33cc8cf72d11fdde2a8af5f18112ede
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2024-11-15 15:00:37 +0545

    Added some action with the button

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -2,16 +2,37 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Clicker extends Component
 {
     public function render()
     {
-        return view('livewire.clicker');
+        $title = "test";
+        $users = User::all();
+        return view('livewire.clicker',[
+            'title' => $title,
+            'users' => $users,
+
+            // return view('livewire.clicker',compact('title'));   
+        ]);
     }
     public function handleClick()
     {
         dump("clicked");
     }
+
+    public function createNewUser()
+    {
+        User::create(
+            [
+                'name' => "test user",
+                'email'=>"test@example.com",
+                'password' => "123123123"
+            ]
+            );
+    }
+
+    public $username = "test user";
 }
--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -2,6 +2,18 @@
     {{-- The whole world belongs to you. --}}
     {{-- Hello Youtube --}}
 
+    <h1>{{$title}}</h1>
 
-    <button type="button" class="btn btn-primary" wire:click="handleClick">Button</button>
+    <h2>{{count($users)}}</h2>
+
+    <h1 class="text-xl">{{$username}}</h1>
+
+    {{-- <button type="button" class="btn btn-primary" wire:click="handleClick">Button</button> --}}
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" wire:click="handleClick">
+        Button
+      </button>
+
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" wire:click="createNewUser">
+        Add New User
+      </button>
 </div>
