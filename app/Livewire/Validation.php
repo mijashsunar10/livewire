98c33b583b8c1942287ff4ac98f3934708121cd3
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2024-11-18 06:59:11 +0545

    Another way to get validated

--- app/Livewire/Validation.php
+++ app/Livewire/Validation.php
@@ -3,24 +3,33 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Validation extends Component
 {
-    public $name;
-    public $email;
-    public $password;
+    #[Rule('required|min:2|max:255')]
+    public $name = '';
+
+    #[Rule('required|email|unique:users,email')]
+    public $email = '';
+
+    #[Rule('required|min:5')]
+    public $password = '';
+
 
     public function createNewUser()
     {
 
-        $this->validate(
-            [
-                'name' => 'required|min:2|max:255',
-                'email' => 'required|email|unique:users,email',
-                'password' => 'required|min:5',
-            ]
-            );
+        // $this->validate(
+        //     [
+        //         'name' => 'required|min:2|max:255',
+        //         'email' => 'required|email|unique:users,email',
+        //         'password' => 'required|min:5',
+        //     ]
+        //     );
+
+        $this->validate();
         
         User::create(
             [
